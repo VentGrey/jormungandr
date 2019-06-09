60a8411e4cee9d30b4db8ceb97a9fdf7228b3534
VentGrey <omarpurataf@gmail.com>
2019-06-09 03:14:44 -0500

    Added Direction object

--- src/snake.rs
+++ src/snake.rs
@@ -6,3 +6,26 @@ use piston_window::types::Color;
 use draw::draw_block;
 
 const SNAKE_COLOUR: Color = [0.00, 0.80, 0.00, 1.0];
+
+// Hora de los objetos
+
+// Enumerado dirección
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+// Métodos para dirección
+impl Direction {
+    pub fn opposite(&self) -> Direction {
+        match *self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
+        }
+    }
+}
+
