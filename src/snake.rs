69386b75a958198ba260769998b0143153f147b0
VentGrey <omarpurataf@gmail.com>
2019-06-09 03:08:11 -0500

    Added snake colour

--- src/snake.rs
+++ src/snake.rs
@@ -2,5 +2,7 @@ use std::collections::LinkedList;
 use piston_window::{Context, G2d};
 use piston_window::types::Color;
 
-// My libs
+// Import local mods
 use draw::draw_block;
+
+const SNAKE_COLOUR: Color = [0.00, 0.80, 0.00, 1.0];
