b3e996741fbd2ce9bfe6a7903000c4afe19b1c8d
VentGrey <omarpurataf@gmail.com>
2019-06-11 01:56:40 -0500

    Added Game structure

--- src/game.rs
+++ src/game.rs
@@ -13,3 +13,18 @@ const GAMEOVER_COLOR: Color = [0.90, 0.00, 0.00, 0.5];
 
 const MOVING_PERIOD: f64 = 0.1;
 const RESTART_TIME: f64 = 1.0;
+
+pub struct Game {
+    snake: Snake,
+
+    food_exists: bool,
+
+    food_x: i32,
+    food_y: i32,
+
+    width: i32,
+    height: i32,
+
+    game_over: bool,
+    waiting_time: f64,
+}
