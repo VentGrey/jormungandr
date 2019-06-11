915c887792cdba9689ceb490e66a9f7e3ea1ae03
VentGrey <omarpurataf@gmail.com>
2019-06-11 02:03:45 -0500

    Mote methods :D

--- src/game.rs
+++ src/game.rs
@@ -42,4 +42,25 @@ impl Game {
             game_over: false
         }
     }
+
+    pub fn key_pressed(&mut self, key: Key) {
+        if self.game_over {
+            return;
+        }
+
+        let dir = match key {
+            Key::Up => Some(Direction::Up),
+            Key::Down => Some(Direction::Down),
+            Key::Left => Some(Direction::Left),
+            Key::Right => Some(Direction::Right),
+            _ => None
+        };
+
+        if dir.unwrap() == self.snake.head_direction().opposite() {
+            return;
+        }
+
+        self.update_snake(dir);
+
+    }
 }
